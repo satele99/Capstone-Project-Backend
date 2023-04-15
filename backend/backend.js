6dc7c90d17321460662c7dcb9ab9aa557a5ea339
Amir Ali <satele.works@gmail.com>
2023-04-15 11:46:14 -0400

    newly updated backend

--- backend/backend.js
+++ backend/backend.js
@@ -4,6 +4,7 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const { Sequelize, DataTypes } = require('sequelize');
+const { create } = require('domain');
 const port = 7000
 const sequelizeServer = new Sequelize('postgres://amirhali:satele@localhost:6000/amirhali', {
     define: {
@@ -14,7 +15,7 @@ const sequelizeServer = new Sequelize('postgres://amirhali:satele@localhost:6000
 app.use(express.json())
 app.use(cors());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -29,7 +30,6 @@ sequelizeServer.authenticate().then(()=> {
 sequelizeServer.sync({alter: true}).then(()=> {
     console.log('tables created successfully')
 });
-sequelizeServer.createSchema('capstone_backend')
 
 
 const User = sequelizeServer.define('users', {
@@ -48,6 +48,14 @@ const User = sequelizeServer.define('users', {
     lastName: {
         type: DataTypes.STRING,
         field: 'last_name'
+    },
+    interests: {
+        type: DataTypes.STRING,
+        field: 'interests'
+    },
+    email: {
+        type: DataTypes.STRING,
+        field: 'user_email'
     }
 }, {
     timestamps: false
@@ -65,3 +73,47 @@ const Posts = sequelizeServer.define('posts', {
 });
 
 //ENDPOINTS VVV
+
+app.post('/create-user', (req, res)=> {
+    const newUser = req.body;
+    async function createUser(){
+        try {
+            const foundUser = await User.findOne({where: {username: newUser.username}})
+            if(!foundUser){
+                User.create({
+                    username: newUser.username,
+                    password: newUser.password,
+                    firstName: newUser.firstName,
+                    lastName: newUser.lastName, 
+                    email: newUser.email, 
+                    interests: 'none'
+                });
+                res.status(200).send('Request successful.')
+            }else{
+                res.status(409).send('Request unsuccessful.')
+            }
+        } catch (error) {
+            if(error){
+                res.status(409).send('Request failed with status code 409')
+            }
+        }
+    }
+    createUser();
+});
+
+app.get('/user/:username', (req, res) => {
+    const username = req.params['username'];
+    async function getUser() {
+        try{
+            const findUser = await User.findOne({where: {username: username}})
+            if(findUser){
+                res.status(200).send(findUser)
+            }
+        } catch (err) {
+            if(err){
+                res.status(409).send('Request failed with status code 409.')
+            }
+        }
+    }
+    getUser();
+})
